7de402788e86efefa310ec153e7d027a79dd7a49
SharAkshay <akshay103@gmail.com>
2022-07-08 21:45:37 +0300

    Initial commit

---
+++ src/main/java/com/supermarket/loyalityprogram/LoyalityprogramApplication.java
@@ -0,0 +1,13 @@
+package com.supermarket.loyalityprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyalityprogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyalityprogramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supermarket/loyalityprogram/LoyalityprogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supermarket.loyalityprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyalityprogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
